4cdcac5b95ef751f52cccb901869e7c5782d18a3
ManSleen <mike.vansleen@gmail.com>
2019-11-05 22:06:20 -0700

    test is passing for merge sort! :)

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -2,41 +2,57 @@
 def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-    # k, j = 0, 0
+
+    # Loop through merged_arr that we just created. for each element...
     for index in range(len(merged_arr)):
+        # make sure both arrays aren't empty
         if len(arrA) > 0 and len(arrB) > 0:
+            # check first element of each array, see which is smaller
+            # If arrA[0] is smaller, add it to merged_arr and delete it from arrA
             if arrA[0] < arrB[0]:
-                print("arrA[0]:", arrA[0])
                 merged_arr[index] = arrA[0]
                 arrA.remove(arrA[0])
+            # If arrB[0] is smaller, add it to merged_arr and delete it from arrB
             else:
-                print("arrB[0]:", arrB[0])
                 merged_arr[index] = arrB[0]
                 arrB.remove(arrB[0])
+        # if either array is empty:
         else:
             if len(arrA) == 0:
+                # is arrA is empty, add the next element from arrB to merged_arr and delete it from arrB
                 merged_arr[index] = arrB[0]
                 arrB.remove(arrB[0])
             else:
+                #  otherwise if arrB is empty, add the next element from arrA and then delete it from arrA
                 merged_arr[index] = arrA[0]
                 arrA.remove(arrA[0])
-
+    print("Merging:", merged_arr)
     return merged_arr
 
 
-print(merge([1, 3, 5, 7, 8, 9, 20, 23, 25, 27], [
-      2, 4, 6, 10, 11, 12, 17, 18, 19, 21, 50, 55, 100, 140]))
+# print(merge([9, 23], [
+#       20]))
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
 
 
 def merge_sort(arr):
-    # TO-DO
+    print("Splitting:", arr)
+    if len(arr) <= 1:
+        return arr
 
-    return arr
+    m = len(arr)//2
+    l = merge_sort(arr[:m])
+    r = merge_sort(arr[m:])
 
+    return merge(l, r)
+
+
+print(merge_sort([25, 1, 7, 3, 5, 8, 20, 23, 9]))
 
 # STRETCH: implement an in-place merge sort algorithm
+
+
 def merge_in_place(arr, start, mid, end):
     # TO-DO
 
